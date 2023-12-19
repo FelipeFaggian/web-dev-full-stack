19325f9ccea15dc3665bda55f186464dbd60b97b
Felipe Faggian <felipesantosfaggian@gmail.com>
2023-12-18 22:02:14 -0300

    drumKitExercise

--- 9 js/1.3 javascript-advanced/1.3.1 drumKitFiles/Drum Kit Starting Files/index.js
+++ 9 js/1.3 javascript-advanced/1.3.1 drumKitFiles/Drum Kit Starting Files/index.js
@@ -1,39 +1,46 @@
-for(var i=0; i < document.querySelectorAll(".drum").length; i++) {
-        document.querySelectorAll("button")[i].addEventListener("click",
-        function() {
-            var buttomPressed = this.innerHTML;
-            switch (buttomPressed) {
-                case "w":
-                    var tom1 = new Audio("./sounds/tom-1.mp3");
-                    tom1.play();
-                    break;
-                case "a":
-                    var tom2 = new Audio("./sounds/tom-2.mp3");
-                    tom2.play();
-                    break;
-                case "s":
-                    var tom3 = new Audio("./sounds/tom-3.mp3");
-                    tom3.play();
-                    break;
-                case "d":
-                    var tom4 = new Audio("./sounds/tom-4.mp3");
-                    tom4.play();
-                    break;
-                case "j":
-                    var snare = new Audio("./sounds/snare.mp3");
-                    snare.play();
-                    break;
-                case "k":
-                    var crash = new Audio("./sounds/crash.mp3");
-                    crash.play();
-                    break;
-                case "l":
-                    var kick = new Audio("./sounds/kick-bass.mp3");
-                    kick.play();
-                    break;
-                default: console.log(buttomPressed);
-                    break;
-            }
-        }
-    )
+document.addEventListener("keydown", keyPress => {
+    var keyboard = keyPress.key;
+    return pressLogic(keyboard); 
+})
+
+for(var i=0; i<document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function() {
+        var mouse = this.innerHTML;
+        return pressLogic(mouse);
+    }
+)}
+
+function pressLogic(pressed) {
+    switch (pressed) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default: console.log();
+            break;
+    }
 }
