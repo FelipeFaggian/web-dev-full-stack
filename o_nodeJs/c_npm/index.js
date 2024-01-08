3543882b4c44a129a70275fbd33089983a5fb343
Felipe Faggian <felipesantosfaggian@gmail.com>
2024-01-07 21:35:02 -0300

    activatingEcmaScript

--- o_nodeJs/c_npm/index.js
+++ o_nodeJs/c_npm/index.js
@@ -1,3 +1,7 @@
-const superHeroName = require('superheroes');
+// const superHeroName = require('superheroes');
 
-console.log("I am " + superHeroName.random() + "!");
+// console.log("I am " + superHeroName.random() + "!");
+
+import superHeroes from 'superheroes';
+
+console.log("I am " + superHeroes.random() + "!");
