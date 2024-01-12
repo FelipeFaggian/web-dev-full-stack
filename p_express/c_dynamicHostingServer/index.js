0cad1ff308f8129a8c92ae2f21da18c2b8fabb34
Felipe Faggian <felipesantosfaggian@gmail.com>
2024-01-12 18:56:56 -0300

    dynamicHosting

---
+++ p_express/c_dynamicHostingServer/index.js
@@ -0,0 +1,19 @@
+//dinamic server (http)
+// import http from "http";
+// import url from "url";
+// import fs from "fs";
+// const port = 3000;
+
+// // http.createServer(function(req, res) {
+// //     var intirePath = url.parse(req.url, true); 
+// //     var fileName = "." + intirePath.pathname;
+// //     fs.readFile(fileName, function(err, data) {
+// //         if (err) throw err;
+    
+// //         res.writeHead(200, {'Content-Type' : 'text/html'});
+// //         res.write(data);
+// //         return res.end();
+// //     });
+// // }).listen(port, () => {
+// //     console.log(`The server is running on port ${port}!`);
+// // });
