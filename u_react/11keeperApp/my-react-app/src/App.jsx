359503161fdef7fce60ae77d0bc01b21604935c7
Felipe Faggian <felipesantosfaggian@gmail.com>
2024-03-27 19:38:47 -0300

    render-notes-keeper-app

--- u_react/11keeperApp/my-react-app/src/App.jsx
+++ u_react/11keeperApp/my-react-app/src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Header from './Header'
-import Footer from './Footer'
-import Note from './Note'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Note from './components/Note'
+import notes from './components/Description'
 
 function App() {
 
   return (
     <div>
-      <Header />
-      <Note />
+      <Header />     
+        {notes.map(Note)}  
       <Footer />
     </div>
   )
